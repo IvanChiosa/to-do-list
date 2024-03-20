29f7bed6d1e4945e6f98cd443aad9505e6b43fdb
Ivan Chiosa <i85chiosa@gmail.com>
2024-03-20 15:36:56 +0100

    Footer.tsx

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+
+            <p>Ivan Chiosa Capstone-Projekt © 2024 neue fische. Alle Rechte vorbehalten.
+                <a href="https://github.com/IvanChiosa"> Github </a>oder
+                <a href="https://www.linkedin.com/in/ivanchiosa/"> Linkedin</a>
+            </p>
+        </footer>
+    );
+};
+export default Footer;
